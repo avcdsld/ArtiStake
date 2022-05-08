7162ee8c6cec4f66128d489967ce263b33809711
氏弘一也 <kazuyaujihiro@gmail.com>
2022-05-08 18:52:13 +0900

    レスポンシブ対応

--- packages/frontend/components/ArtistWorks/ArtistWorks.tsx
+++ packages/frontend/components/ArtistWorks/ArtistWorks.tsx
@@ -12,25 +12,25 @@ const ArtistWorks: React.FC<ArtistWorksProps> = ({
 }) => {
   return (
     <div className="w-full mx-auto grid lg:grid-cols-3">
-      <div className="bg-marimo-2 p-4">
+      <div className="bg-marimo-2 py-4 lg:p-4">
         <p className="text-center text-black text-xl">Project Gallery</p>
-        <div className="p-8">
+        <div className="py-8 lg:p-8">
           <a href={galleryUrl} target="_blank" rel="noreferrer">
             <img className="mx-auto h-48 object-cover" src={galleryTumbnail} />
           </a>
         </div>
       </div>
-      <div className="bg-marimo-1 p-4">
+      <div className="bg-marimo-1 py-4 lg:p-4">
         <p className="text-center text-black text-xl">Stake in Metaverse</p>
-        <div className="p-8">
+        <div className="py-8 lg:p-8">
           <a href={shopUrl} target="_blank" rel="noreferrer">
             <img className="mx-auto h-48 object-cover" src={shopTumbnail} />
           </a>
         </div>
       </div>
-      <div className="bg-marimo-2 p-4">
+      <div className="bg-marimo-2 py-4 lg:p-4">
         <p className="text-center text-black text-xl">Tip The Project</p>
-        <div className="flex justify-center p-8">
+        <div className="flex justify-center py-8 lg:p-8">
           <Tip artistWalletAddress={walletAddress} />
         </div>
       </div>
